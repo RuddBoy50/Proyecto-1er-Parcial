700d5e1b1e003a8b13c74ebdbc0be69e0fc52dc7
Dorian Ruddy Medina Arcos <medinaruddy70@gmail.com>
2024-02-24 18:23:26 -0600

    Actualizacion de algunos documentos

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/framework/public/';
+    public string $baseURL = 'http://localhost/ProyectoChucho/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
