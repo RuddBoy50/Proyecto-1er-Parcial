4435f14ea3e496f8ffb19c1f192d9812def400d5
Dorian Ruddy Medina Arcos <medinaruddy70@gmail.com>
2024-02-25 23:35:56 -0600

    Nuevos Estilos de paginas

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -93,4 +93,4 @@ define('EVENT_PRIORITY_NORMAL', 100);
  */
 define('EVENT_PRIORITY_HIGH', 10);
 
-define('BASE_URL', 'http://localhost/ProyectoChuco/public/');
+define('BASE_URL', 'http://localhost/ProyectoChucho/public');
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'ProyectoChucho',
+        'database'     => 'proyectochucho',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
